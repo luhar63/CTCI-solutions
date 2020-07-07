852671c73ae3f859cf515e0afc80e3f227752750
Rahul Maurya <rahulmaurya.mnnit@gmail.com>
2020-07-06 20:22:01 -0400

    Linked list question:2.3

---
+++ LinkedList/2.3_deleteMiddleNode.py
@@ -0,0 +1,61 @@
+"""
+Delete the middle node
+"""
+import random
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def getLength(head):
+    length = 0
+    while head:
+        head = head.next
+        length += 1
+    return length
+
+
+"""
+Time Complexity: O(N)
+Space Complexity: O(1)
+"""
+
+
+def deleteMiddleNode(node):
+    node.val = node.next.val
+    node.next = node.next.next
+
+
+# driver function
+if __name__ == "__main__":
+    ls = [4, 3, 5, 6, 7, 3, 2, 1]
+    head = None
+    cur = None
+    for v in ls:
+        if not head:
+            head = ListNode(v)
+            cur = head
+        else:
+            n = ListNode(v)
+            cur.next = n
+            cur = cur.next
+
+    num = random.randrange(1, len(ls) - 1)
+    cur = head
+    while num > 0:
+        cur = cur.next
+        num -= 1
+
+    print("delete : ", cur.val)
+    deleteMiddleNode(cur)
+
+    res = []
+    cur = head
+    while cur:
+        res.append(cur.val)
+        cur = cur.next
+
+    print(res)
