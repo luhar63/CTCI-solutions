15d10dd72245eae7e73f6dd331e254fbf2f22d46
Rahul Maurya <rahulmaurya.mnnit@gmail.com>
2020-08-07 20:08:35 -0400

    4.1 - updated edge case

--- Trees_And_Graphs/4.1_route_between_nodes.py
+++ Trees_And_Graphs/4.1_route_between_nodes.py
@@ -4,21 +4,25 @@ Route b/w nodes
 
 
 def routeBetweenNodes(numberOfNodes, adjacencyList, source, destination):
+    if source == destination:
+        return True
     queue = []
-    visited = [False] * (numberOfNodes + 1)
+    visited = [0] * (numberOfNodes + 1)  # 0-unvisited, 1- visiting, 2 - visited
 
     queue.append(source)
 
     while len(queue) > 0:
         u = queue.pop(0)
-        visited[u] = True
+        visited[u] = 1
         if u not in adjacencyList:
             continue
         for v in adjacencyList[u]:
-            if v == destination:
-                return True
-            if not visited[v]:
-                queue.append(v)
+            if visited[v] == 0:
+                if v == destination:
+                    return True
+                else:
+                    queue.append(v)
+        visited[u] = 2
     return False
 
 
